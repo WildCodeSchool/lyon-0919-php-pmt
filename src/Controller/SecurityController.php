fa24f49855b7c465030f243a41a8989928d1cbff
Pierre-Alain Monney <pierrealain.monney@gmail.com>
2019-12-07 22:35:48 +0100

    mise a jour de la route du formulaire de pré inscriptioné

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -22,4 +22,11 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername
         ]);
     }
+
+    /**
+     * @Route("/logout", name= "security_logout")
+     */
+    public function logout()
+    {
+    }
 }
