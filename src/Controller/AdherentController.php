d3c172ab6b793bcabebb667a2c1e43539e05ba4d
julien <julienl6900669006@gmail.com>
2019-12-11 09:12:46 +0100

    affichage de la page adherent

---
+++ src/Controller/AdherentController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdherentController extends AbstractController
+{
+    /**
+     * @Route("/account", name="account_index", methods={"GET"})
+     */
+    public function show(): Response
+    {
+//
+//        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+//
+//        // returns your User object, or null if the user is not authenticated
+//        // use inline documentation to tell your editor your exact User class
+//        /** @var \App\Entity\User $user */
+//        $user = $this->getUser();
+
+
+        return $this->render('user/show.html.twig', [
+//            'user' => $user,
+        ]);
+    }
+}
