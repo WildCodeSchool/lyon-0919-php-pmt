ce36a4d7e48d18b64ec470b271b7dc53c91c0bde
julien <julienl6900669006@gmail.com>
2020-01-07 13:28:00 +0100

    fix travis beug

--- src/Entity/Inscription.php
+++ src/Entity/Inscription.php
@@ -78,7 +78,7 @@ class Inscription
 
     public function __toString(): string
     {
-        return $this->getstatus();
+        return strval($this->getstatus());
     }
 
     public function getId(): ?int
