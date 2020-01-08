c492a0dfee8f81201e96cca119d08b7ecf273239
emeric <marion.emeric@laposte.net>
2020-01-08 15:45:55 +0100

    Modification image user by default

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use DateTime;
 use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -165,6 +166,10 @@ class User implements UserInterface, \Serializable
 
     public function __construct()
     {
+        if ($this->getCreatedAt() === null) {
+            $this->setCreatedAt(new DateTime('now'));
+            $this->setUpdateAt(new DateTime('now'));
+        }
         $this->inscriptions = new ArrayCollection();
         $this->participants = new ArrayCollection();
     }
