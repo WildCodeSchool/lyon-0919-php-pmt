edda1a6fc09d2eb21e738d50380f9439b2809313
julien <julienl6900669006@gmail.com>
2020-01-10 10:35:39 +0100

    fix travis erreur

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -588,7 +588,7 @@ class User implements UserInterface, \Serializable
         if (null !== $imageFile) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->updatedAt() = new DateTime();
+            $this->updatedAt( new DateTime());
         }
     }
 
