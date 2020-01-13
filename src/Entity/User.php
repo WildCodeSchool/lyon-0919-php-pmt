0ef4d8c258bab5e35da99c06293a7fc334bd05e4
emeric <marion.emeric@laposte.net>
2020-01-13 14:45:32 +0100

    modif ImageName for Travis

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -75,7 +75,7 @@ class User implements UserInterface, \Serializable
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
-     * @var string
+     * @var ?string
      */
     private $imageName = "logo_PMT.png";
 
