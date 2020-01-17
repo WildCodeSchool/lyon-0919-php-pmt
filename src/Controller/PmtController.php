ff020538c402f3ad759f1b355c611cd1a9da84b8
philippedu69 <philippeperrin1968@free.fr>
2020-01-17 10:26:01 +0100

    affichage dynamique team

--- src/Controller/PmtController.php
+++ src/Controller/PmtController.php
@@ -11,9 +11,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-
-
 /**
  *
  * @Route("/", name="pmt")
